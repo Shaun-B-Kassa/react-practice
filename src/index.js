447b55660b39dde3a379d83a6a4b4b4008a102f0
Shaun Kassa <35867458+Shaun-B-Kassa@users.noreply.github.com>
2020-03-07 23:33:58 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import app from './App';
+
+const rootelement = document.getElementById('root');
+
+ReactDOM.render(
+  <React.StrictMode>
+    <app/>
+  </React.StrictMode>,
+  rootelement
+);
+
